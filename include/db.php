3ac93a07c28edc05e0ed44bed310e514ceabef62
Sdeveloper <developer.saqlain007@gmail.com>
2022-06-15 18:06:16 +0600

    commit

--- include/db.php
+++ include/db.php
@@ -2,6 +2,6 @@
 $db=mysqli_connect("remotemysql.com","AI12KXeUaX","Lm35bRUnWu","AI12KXeUaX");
 if($db){
     echo "databse is connected !";
-}else{
-   echo "something is wrong with database !";
+//}else{
+   //echo "something is wrong with database !";
 }
